05fb61c5d7d4b1b3c932272a0c82034758852fae
hoangnoproo2k3 <hoangproo2624@gmail.com>
2023-02-25 01:08:15 +0700

    Config project Router and build layout loading

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+//Layouts
+import { HeaderOnly } from "~/components/Layout";
+//Pages
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+import Search from "~/pages/Search";
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout_page: HeaderOnly },
+    { path: '/search', component: Search, layout_page: null },
+]
+const privateRoutes = [
+
+]
+export { publicRoutes, privateRoutes };
